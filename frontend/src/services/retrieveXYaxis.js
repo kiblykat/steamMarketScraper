1e5a3b272e25595a9b47d3dde3e3b8e75dffc673
izzat <izzatfadzlon@gmail.com>
2024-12-28 11:06:04 +0800

    fe: update timestamp formatting using regex in retrieveXYaxis service

--- frontend/src/services/retrieveXYaxis.js
+++ frontend/src/services/retrieveXYaxis.js
@@ -6,7 +6,9 @@ export const retrieveXYaxis = (data) => {
     let currTimestamps = datapointMap.get("timestamps");
     let currPrices = datapointMap.get("prices");
     let currTotalSales = datapointMap.get("totalSales");
-    currTimestamps ? currTimestamps.push(time) : (currTimestamps = [time]);
+    currTimestamps
+      ? currTimestamps.push(time.replace(/^([A-Za-z]{3} \d{2}).*$/, "$1"))
+      : (currTimestamps = [time.replace(/^([A-Za-z]{3} \d{2}).*$/, "$1")]);
     currPrices ? currPrices.push(price) : (currPrices = [price]);
     currTotalSales ? currTotalSales.push(sales) : (currTotalSales = [sales]);
 
