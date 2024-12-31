2d6a96074c849ae60329396d85d43d612a88fbe0
izzat <izzatfadzlon@gmail.com>
2024-12-31 18:31:24 +0800

    be: add new case retrieval endpoints for various weapon cases

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ import {
   retrievePrisma2Case,
   retrievePrismaCase,
   retrieveSnakebiteCase,
+  retrieveFalchionCase,
+  retrieveGammaCase,
+  retrieveGloveCase,
+  retrieveHuntsmanWeaponCase,
+  retrieveOperationBreakoutWeaponCase,
+  retrieveOperationBrokenFangCase,
+  retrieveOperationHydraCase,
+  retrieveOperationRiptideCase,
+  retrieveShadowCase,
+  retrieveWinterOffensiveWeaponCase,
 } from "./routes/caseRoutes.js";
 import { retrieveMarketAnalysis } from "./routes/analyzePriceMarketRoute.js";
 
@@ -24,6 +34,25 @@ app.get("/api/data/prisma-2-case", retrievePrisma2Case);
 app.get("/api/data/prisma-case", retrievePrismaCase);
 app.get("/api/data/snakebite-case", retrieveSnakebiteCase);
 app.get("/api/data/chroma-case", retrieveChromaCase);
+app.get("/api/data/falchion-case", retrieveFalchionCase);
+app.get("/api/data/gamma-case", retrieveGammaCase);
+app.get("/api/data/glove-case", retrieveGloveCase);
+app.get("/api/data/huntsman-weapon-case", retrieveHuntsmanWeaponCase);
+app.get(
+  "/api/data/operation-breakout-weapon-case",
+  retrieveOperationBreakoutWeaponCase
+);
+app.get(
+  "/api/data/operation-broken-fang-case",
+  retrieveOperationBrokenFangCase
+);
+app.get("/api/data/operation-hydra-case", retrieveOperationHydraCase);
+app.get("/api/data/operation-riptide-case", retrieveOperationRiptideCase);
+app.get("/api/data/shadow-case", retrieveShadowCase);
+app.get(
+  "/api/data/winter-offensive-weapon-case",
+  retrieveWinterOffensiveWeaponCase
+);
 
 app.get("/api/data/analyze-price-market", retrieveMarketAnalysis);
 
