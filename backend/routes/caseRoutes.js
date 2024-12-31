1d878b7da6be161f612d076e18122f24df0fe1c2
izzat <izzatfadzlon@gmail.com>
2024-12-31 17:57:20 +0800

    be: add new case retrieval endpoints for various weapon cases

--- backend/routes/caseRoutes.js
+++ backend/routes/caseRoutes.js
@@ -6,6 +6,16 @@ import { Prisma2Case } from "../marketPriceHistory/Prisma2Case.js";
 import { PrismaCase } from "../marketPriceHistory/PrismaCase.js";
 import { SnakebiteCase } from "../marketPriceHistory/SnakebiteCase.js";
 import { ChromaCase } from "../marketPriceHistory/ChromaCase.js";
+import { FalchionCase } from "../marketPriceHistory/FalchionCase.js";
+import { GammaCase } from "../marketPriceHistory/GammaCase.js";
+import { GloveCase } from "../marketPriceHistory/GloveCase.js";
+import { HuntsmanWeaponCase } from "../marketPriceHistory/HuntsmanWeaponCase.js";
+import { OperationBreakoutWeaponCase } from "../marketPriceHistory/OperationBreakoutWeaponCase.js";
+import { OperationBrokenFangCase } from "../marketPriceHistory/OperationBrokenFangCase.js";
+import { OperationHydraCase } from "../marketPriceHistory/OperationHydraCase.js";
+import { OperationRiptideCase } from "../marketPriceHistory/OperationRiptideCase.js";
+import { ShadowCase } from "../marketPriceHistory/ShadowCase.js";
+import { WinterOffensiveWeaponCase } from "../marketPriceHistory/WinterOffensiveWeaponCase-do not use.js";
 
 export const retrieveCS20Case = (req, res) => {
   res.status(200).json(CS20Case);
@@ -30,4 +40,34 @@ export const retrieveSnakebiteCase = (req, res) => {
 };
 export const retrieveChromaCase = (req, res) => {
   res.status(200).json(ChromaCase);
+};
+export const retrieveFalchionCase = (req, res) => {
+  res.status(200).json(FalchionCase);
+};
+export const retrieveGammaCase = (req, res) => {
+  res.status(200).json(GammaCase);
+};
+export const retrieveGloveCase = (req, res) => {
+  res.status(200).json(GloveCase);
+};
+export const retrieveHuntsmanWeaponCase = (req, res) => {
+  res.status(200).json(HuntsmanWeaponCase);
+};
+export const retrieveOperationBreakoutWeaponCase = (req, res) => {
+  res.status(200).json(OperationBreakoutWeaponCase);
+};
+export const retrieveOperationBrokenFangCase = (req, res) => {
+  res.status(200).json(OperationBrokenFangCase);
+};
+export const retrieveOperationHydraCase = (req, res) => {
+  res.status(200).json(OperationHydraCase);
+};
+export const retrieveOperationRiptideCase = (req, res) => {
+  res.status(200).json(OperationRiptideCase);
+};
+export const retrieveShadowCase = (req, res) => {
+  res.status(200).json(ShadowCase);
+};
+export const retrieveWinterOffensiveWeaponCase = (req, res) => {
+  res.status(200).json(WinterOffensiveWeaponCase);
 };
